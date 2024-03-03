55599e477144cae84324dbf018e0d0bce3a0ae6b
readysloth <readysloth@protonmail.com>
2024-03-03 22:41:30 +0300

    add(mitmmock): Dump and run addons to mitmproxy

---
+++ mitmmock/dump.py
@@ -0,0 +1,71 @@
+import json
+
+from collections.abc import Iterable
+
+from mitmproxy import ctx
+from mitmproxy.http import HTTPFlow
+from mitmproxy.addonmanager import Loader
+
+
+def decode_bytes_in_state(obj: dict):
+    def decode_value(value):
+        if type(value) == bytes:
+            return value.decode()
+        if type(value) != str and isinstance(value, Iterable):
+            return [decode_value(v) for v in value]
+        return value
+    return {k: decode_value(v) for k, v in obj.items()}
+
+
+class MitmMockConfigDump:
+    def __init__(self):
+        self.chain = []
+
+    def load(self, loader: Loader):
+        loader.add_option(
+            name='mitmmock_dump',
+            typespec=str,
+            default='mitmmock.dump.json',
+            help='File to serialize flows'
+        )
+
+    def request(self, flow: HTTPFlow):
+        self.chain.append(
+            {'Request': decode_bytes_in_state(flow.request.get_state())}
+        )
+
+    def response(self, flow: HTTPFlow):
+        self.chain.append(
+            {'Response': decode_bytes_in_state(flow.response.get_state())}
+        )
+
+    def done(self):
+        with open(ctx.options.mitmmock_dump, 'w') as f:
+            header = r'''{{def(postproc_left, postproc_right)}}
+{{if true == false}}
+    This is comment block.
+    Requests, for now, displayed here only as context for the user.
+
+    MitmMock execution is splitted in two parts:
+        - load time -- preprocessing of json
+        - runtime -- processing of json keys/values
+
+    To modify responses in load time, you can use preppy as it stated
+    in preppy's documentation (see https://preppy.readthedocs.io)
+
+    To modify responses in runtime, you should use functions defined
+    below:
+        - runtime escapes expression, so it would not affect preppy
+        in the load time
+        - imp escapes `import` template, so it would not affect preppy
+        in the load time
+{{endif}}
+
+{{def runtime(expr)}}{{postproc_left}}{{expr}}{{postproc_right}}{{enddef}}
+{{def imp(module)}}{{postproc_left}}import {{module}}{{postproc_right}}{{enddef}}
+'''
+            f.write(header)
+            f.write(json.dumps(self.chain, indent=2))
+
+
+addons = [MitmMockConfigDump()]
---
+++ mitmmock/mitmmock.py
@@ -0,0 +1,66 @@
+import json
+
+import preppy
+
+from collections.abc import Iterable
+
+from mitmproxy import ctx
+from mitmproxy.http import HTTPFlow, Response, Headers
+from mitmproxy.addonmanager import Loader
+
+
+class MitmMock:
+    def __init__(self):
+        self.chain = []
+
+    def load(self, loader: Loader):
+        loader.add_option(
+            name='mitmmock_dump',
+            typespec=str,
+            default='mitmmock.dump.json',
+            help='File to load serialized flows'
+        )
+
+    def configure(self, updates: set):
+        if 'mitmmock_dump' in updates:
+            self.mitmmock_dump_path = ctx.options.mitmmock_dump
+            with open(self.mitmmock_dump_path, 'r') as mitmmockflows:
+                self.mitmmockflows_raw = mitmmockflows.read()
+
+            self.initial_prep = preppy.getModule('mitmmock',
+                                                 sourcetext=self.mitmmockflows_raw,
+                                                 source_extension=None)
+            self.initial_json = self.initial_prep.get('{{', '}}')
+            self.chain = json.loads(self.initial_json)
+            self.requests = [e['Request'] for e in self.chain if 'Request' in e]
+            self.responses = [e['Response'] for e in self.chain if 'Response' in e]
+            self.ptr = 0
+
+    def eval_preppy_value(self, value):
+        if type(value) == str:
+            return preppy.getModule('mm_part',
+                                    sourcetext=value,
+                                    source_extension=None).getOutput({})
+        if type(value) != str and isinstance(value, Iterable):
+            return [self.eval_preppy_value(v) for v in value]
+        return value
+
+    def eval_preppy(self, obj: dict):
+        return {self.eval_preppy_value(k): self.eval_preppy_value(v) for k, v in obj.items()}
+
+    def request(self, flow: HTTPFlow):
+        saved_resp = self.responses[self.ptr]
+        headers = Headers()
+        for k, v in saved_resp['headers']:
+            headers[self.eval_preppy_value(k)] = self.eval_preppy_value(v)
+        saved_resp['headers'] = headers
+
+        flow.response = Response.make(status_code=saved_resp['status_code'],
+                                      content=saved_resp['content'],
+                                      headers=saved_resp['headers'])
+        flow.is_replay = "response"
+        self.ptr += 1
+        self.ptr %= len(self.responses)
+
+
+addons = [MitmMock()]
